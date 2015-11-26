59cae0749e318a14e3d07074676461e52e6a615b
FuriousLogic <mrbarrydonaldson@gmail.com>
2015-11-26 17:15:13 +0000

    StepDependancy tests

--- YesChef_DataLayer.Tests/StepDependancyTests.cs
+++ YesChef_DataLayer.Tests/StepDependancyTests.cs
@@ -5,41 +5,44 @@ using NUnit.Framework;
 namespace YesChef_DataLayer.Tests
 {
     [TestFixture]
-    [Ignore]
     public class StepDependancyTests
     {
-        //[Test]
-        //public void ShouldCreateStepDependancy()
-        //{
-        //    var recipe = RecipeHandler.CreateRecipe($"recipeName {Guid.NewGuid()}");
-        //    Assert.That(recipe, !Is.Null);
-        //    var step1 = StepHandler.CreateStep($"step description {Guid.NewGuid()}", 1, recipe.Id);
-        //    Assert.That(step1, !Is.Null);
-        //    var step2 = StepHandler.CreateStep($"step description {Guid.NewGuid()}", 1, recipe.Id);
-        //    Assert.That(step2, !Is.Null);
+        [Test]
+        public void ShouldCreateStepDependancy()
+        {
+            var recipe = RecipeHandler.CreateRecipe($"recipeName {Guid.NewGuid()}");
+            Assert.That(recipe, !Is.Null);
+            var step1 = StepHandler.CreateStep($"step description {Guid.NewGuid()}", 1, recipe.Id);
+            Assert.That(step1, !Is.Null);
+            Assert.That(step1.Recipe, Is.Not.Null);
+            var step2 = StepHandler.CreateStep($"step description {Guid.NewGuid()}", 1, recipe.Id);
+            Assert.That(step2, !Is.Null);
+            Assert.That(step2.Recipe, Is.Not.Null);
 
-        //    var stepDependancy = StepDependancyHandler.CreateStepDependancy(step1.Id, step2.Id);
-        //    Assert.That(stepDependancy, !Is.Null);
-        //    Assert.That(stepDependancy.Id, Is.GreaterThan(0));
+            var stepDependancy = StepDependancyHandler.CreateStepDependancy(step1.Id, step2.Id);
+            Assert.That(stepDependancy, !Is.Null);
+            Assert.That(stepDependancy.Id, Is.GreaterThan(0));
 
-        //    Assert.That(stepDependancy.ParentStep.Id, Is.EqualTo(step1.Id));
-        //    Assert.That(stepDependancy.ChildStep.Id, Is.EqualTo(step2.Id));
-        //}
+            Assert.That(stepDependancy.ParentStepId, Is.EqualTo(step1.Id));
+            Assert.That(stepDependancy.ChildStepId, Is.EqualTo(step2.Id));
+        }
 
-        //[Test]
-        //public void ShouldNotCreateStepDependancyASecondTime()
-        //{
-        //    var recipe = RecipeHandler.CreateRecipe($"recipeName {Guid.NewGuid()}");
-        //    Assert.That(recipe, !Is.Null);
-        //    var step1 = StepHandler.CreateStep($"step description {Guid.NewGuid()}", 1, recipe.Id);
-        //    Assert.That(step1, !Is.Null);
-        //    var step2 = StepHandler.CreateStep($"step description {Guid.NewGuid()}", 1, recipe.Id);
-        //    Assert.That(step2, !Is.Null);
+        [Test]
+        public void ShouldNotCreateStepDependancyASecondTime()
+        {
+            var recipe = RecipeHandler.CreateRecipe($"recipeName {Guid.NewGuid()}");
+            Assert.That(recipe, !Is.Null);
+            var step1 = StepHandler.CreateStep($"step description {Guid.NewGuid()}", 1, recipe.Id);
+            Assert.That(step1, !Is.Null);
+            Assert.That(step1.Recipe, Is.Not.Null);
+            var step2 = StepHandler.CreateStep($"step description {Guid.NewGuid()}", 1, recipe.Id);
+            Assert.That(step2, !Is.Null);
+            Assert.That(step2.Recipe, Is.Not.Null);
 
-        //    var stepDependancy = StepDependancyHandler.CreateStepDependancy(step1.Id, step2.Id);
-        //    Assert.That(stepDependancy, !Is.Null);
+            var stepDependancy = StepDependancyHandler.CreateStepDependancy(step1.Id, step2.Id);
+            Assert.That(stepDependancy, Is.Not.Null);
 
-        //    Assert.That(() => StepDependancyHandler.CreateStepDependancy(step1.Id, step2.Id), Throws.Exception);
-        //}
+            Assert.That(() => StepDependancyHandler.CreateStepDependancy(step1.Id, step2.Id), Throws.Exception);
+        }
     }
 }
