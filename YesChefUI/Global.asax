7ff2c45931d7d14d1d1e736801b9de1fa02c5eb0
FuriousLogic <mrbarrydonaldson@gmail.com>
2016-01-13 18:34:25 +0000

    Email confirmed login

---
+++ YesChefUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YesChefUI.MvcApplication" Language="C#" %>
--- YesChefUI/Properties/AssemblyInfo.cs
+++ YesChefUI/Properties/AssemblyInfo.cs
@@ -2,35 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("YesChef")]
+[assembly: AssemblyTitle("YesChefUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Catlin")]
-[assembly: AssemblyProduct("YesChef")]
-[assembly: AssemblyCopyright("Copyright © Catlin 2015")]
+[assembly: AssemblyProduct("YesChefUI")]
+[assembly: AssemblyCopyright("Copyright © Catlin 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fa9d9fc9-f292-47d8-925b-93484313941a")]
+[assembly: Guid("bd681518-b3ea-43a6-b39c-b6de789869ee")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
